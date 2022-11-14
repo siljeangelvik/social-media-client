6abb11e2010a44d17cb4a759e6c01fbdbadafb66
silje <angelviksilje@gmail.com>
2022-11-14 18:09:32 +0100

    trying to get a successful deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 export default {
-  base: "/social-media-client-angelvik/",
+  base: "/social-media-client/",
   root: path.resolve(__dirname, "./"),
   resolve: {
     alias: {
